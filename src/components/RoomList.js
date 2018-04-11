433d6b984a854af280c3e82ea81ee57756fcb7a8
Jacob <jake.laurenti@gmail.com>
2018-04-11 00:13:35 -0400

    create room functionality

--- src/components/RoomList.js
+++ src/components/RoomList.js
@@ -3,11 +3,21 @@ import React, { Component } from 'react';
 class RoomList extends Component {
   constructor(props) {
     super(props);
-      this.state = {
-        rooms: []
-      };
+      this.state = { name: "", rooms: [] };
 
+    this.handleChange = this.handleChange.bind(this);
+    this.createRoom = this.createRoom.bind(this);
     this.roomsRef = this.props.firebase.database().ref('rooms');
+  }
+
+  handleChange(e) {
+    this.setState({ name: e.target.value });
+  }
+
+  createRoom(e) {
+    e.preventDefault();
+    this.roomsRef.push({ name: this.state.name });
+    this.setState({ name: "" });
   }
 
   componentDidMount() {
@@ -20,11 +30,22 @@ class RoomList extends Component {
 
 
   render() {
+    const roomForm = (
+      <form onSubmit={this.createRoom}>
+        <input type="text" value={this.state.name} placeholder="Enter Room Name" onChange={this.handleChange}/>
+        <input type="submit" value="Create" />
+      </form>
+    );
+
     const roomList = this.state.rooms.map((room) =>
       <li key={room.key}>{room.name}</li>
     );
+
     return(
-      <ul>{roomList}</ul>
+      <div>
+        <div>{roomForm}</div>
+        <ul>{roomList}</ul>
+      </div>
     );
   }
 }
